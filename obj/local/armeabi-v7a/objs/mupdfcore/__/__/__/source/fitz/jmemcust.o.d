8b1352d1c28db17b962c8698b4200a69d40b6c1b
Nathaniel Jobs <nathaniel7687@gmail.com>
2015-11-18 14:44:39 +0900

    Initial commit

---
+++ obj/local/armeabi-v7a/objs/mupdfcore/__/__/__/source/fitz/jmemcust.o.d
@@ -0,0 +1,89 @@
+obj/local/armeabi-v7a/objs/mupdfcore/__/__/__/source/fitz/jmemcust.o: \
+ jni/../../../source/fitz/jmemcust.c ../../thirdparty/jpeg/jinclude.h \
+ ../../scripts/jpeg/jconfig.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/memory.h \
+ /cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdio.h \
+ ../../thirdparty/jpeg/jpeglib.h ../../thirdparty/jpeg/jmorecfg.h \
+ ../../thirdparty/jpeg/jmemsys.h ../../thirdparty/jpeg/jerror.h \
+ jni/../../../source/fitz/jmemcust.h
+
+../../thirdparty/jpeg/jinclude.h:
+
+../../scripts/jpeg/jconfig.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/DevelopTools/AndroidSDK/ndk-bundle/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+../../thirdparty/jpeg/jpeglib.h:
+
+../../thirdparty/jpeg/jmorecfg.h:
+
+../../thirdparty/jpeg/jmemsys.h:
+
+../../thirdparty/jpeg/jerror.h:
+
+jni/../../../source/fitz/jmemcust.h:
